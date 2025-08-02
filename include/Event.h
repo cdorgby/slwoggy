9c37900dac40f780bee6e8811ccae2cec0f0ce7b
cdorgby <cdorgby@dorgby.net>
2025-08-02 07:29:55 -0400

    Remove old stuff

--- include/Event.h
+++
@@ -1,7 +0,0 @@
-#ifndef EVENT_H
-#define EVENT_H
-
-// This file is kept minimal as we've moved to a state-based input system.
-// Scene transitions are now handled directly without event objects.
-
-#endif // EVENT_H
