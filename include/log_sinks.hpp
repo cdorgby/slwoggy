479de8850cabcf81093393f6feed788e4dcdcc3d
cdorgby <83705463+cdorgby@users.noreply.github.com>
2025-08-22 11:40:01 +0000

    Refactor log sink factory functions to use designated initializers for clarity

--- include/log_sinks.hpp
+++ include/log_sinks.hpp
@@ -21,42 +21,96 @@ namespace slwoggy
 template <typename Filter = no_filter> inline std::shared_ptr<log_sink> make_stdout_sink(Filter &&filter = {})
 {
     if constexpr (std::is_same_v<std::decay_t<Filter>, no_filter>)
-        return std::make_shared<log_sink>(raw_formatter{true, true}, file_writer{STDOUT_FILENO});
+    {
+        return std::make_shared<log_sink>(
+            raw_formatter{
+                .use_color   = true,
+                .add_newline = true,
+            },
+            file_writer{STDOUT_FILENO});
+    }
     else
-        return std::make_shared<log_sink>(raw_formatter{true, true}, file_writer{STDOUT_FILENO}, std::forward<Filter>(filter));
+    {
+        return std::make_shared<log_sink>(
+            raw_formatter{
+                .use_color   = true,
+                .add_newline = true,
+            },
+            file_writer{STDOUT_FILENO},
+            std::forward<Filter>(filter));
+    }
 }
 
 template <typename Filter = no_filter>
 inline std::shared_ptr<log_sink> make_raw_file_sink(const std::string_view &filename, rotate_policy policy = {}, Filter &&filter = {})
 {
     if constexpr (std::is_same_v<std::decay_t<Filter>, no_filter>)
-        return std::make_shared<log_sink>(raw_formatter{false, true}, file_writer{std::string(filename), policy});
+    {
+        return std::make_shared<log_sink>(
+            raw_formatter{
+                .use_color   = false,
+                .add_newline = true,
+            },
+            file_writer{std::string(filename), policy});
+    }
     else
-        return std::make_shared<log_sink>(raw_formatter{false, true},
-                                          file_writer{std::string(filename), policy},
-                                          std::forward<Filter>(filter));
+    {
+        return std::make_shared<log_sink>(
+            raw_formatter{
+                .use_color   = false,
+                .add_newline = true,
+            },
+            file_writer{std::string(filename), policy},
+            std::forward<Filter>(filter));
+    }
 }
 
 template <typename Filter = no_filter>
 inline std::shared_ptr<log_sink> make_writev_file_sink(const std::string_view &filename, rotate_policy policy = {}, Filter &&filter = {})
 {
     if constexpr (std::is_same_v<std::decay_t<Filter>, no_filter>)
-        return std::make_shared<log_sink>(raw_formatter{false, true}, writev_file_writer{std::string(filename), policy});
+    {
+        return std::make_shared<log_sink>(
+            raw_formatter{
+                .use_color   = false,
+                .add_newline = true,
+            },
+            writev_file_writer{std::string(filename), policy});
+    }
     else
-        return std::make_shared<log_sink>(raw_formatter{false, true},
-                                          writev_file_writer{std::string(filename), policy},
-                                          std::forward<Filter>(filter));
+    {
+        return std::make_shared<log_sink>(
+            raw_formatter{
+                .use_color   = false,
+                .add_newline = true,
+            },
+            writev_file_writer{std::string(filename), policy},
+            std::forward<Filter>(filter));
+    }
 }
 
 template <typename Filter = no_filter>
 inline std::shared_ptr<log_sink> make_json_sink(const std::string_view &filename, rotate_policy policy = {}, Filter &&filter = {})
 {
     if constexpr (std::is_same_v<std::decay_t<Filter>, no_filter>)
-        return std::make_shared<log_sink>(taocpp_json_formatter{false, true}, file_writer{std::string(filename), policy});
+    {
+        return std::make_shared<log_sink>(
+            taocpp_json_formatter{
+                .pretty_print = false,
+                .add_newline  = true,
+            },
+            file_writer{std::string(filename), policy});
+    }
     else
-        return std::make_shared<log_sink>(taocpp_json_formatter{false, true},
-                                          file_writer{std::string(filename), policy},
-                                          std::forward<Filter>(filter));
+    {
+        return std::make_shared<log_sink>(
+            taocpp_json_formatter{
+                .pretty_print = false,
+                .add_newline  = true,
+            },
+            file_writer{std::string(filename), policy},
+            std::forward<Filter>(filter));
+    }
 }
 
 } // namespace slwoggy
